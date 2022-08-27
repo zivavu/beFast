7b58d6de35070eea532148dca17d9382dc9095a8
Tomasz Kierzenkowski <107223633+zivavu@users.noreply.github.com>
2022-08-27 21:48:49 +0200

    first commit

---
+++ wordsToDom.js
@@ -0,0 +1,5 @@
+const outputArea = document.getElementById('output-area');
+
+let words =
+	'się i w nie na z do to że a o jak ale po co tak za od go już jego jej czy przez tylko tego sobie jeszcze może ze kiedy pan ich dla by gdy teraz ja ten który nawet bardzo przed tu jednak pod coś tam wszystko przy więc nic bo nim żeby miał on być potem też jeśli bez nad gdzie lecz siebie nigdy ani właśnie sam u dobrze niż jakby aby ty oczy zawsze raz były no albo gdyby aż wtedy przecież ona drzwi jako chyba nagle wszyscy jeden czym kto sposób czas kilka dlaczego razem';
+words = words.split(' ');
